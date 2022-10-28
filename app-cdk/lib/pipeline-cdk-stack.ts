50e987e5bfaa15eca2eb9cf6106f95c99caef4ad
lauradaybell <laura.l.daybell@gmail.com>
2022-10-28 18:20:28 +0000

    updates

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -3,12 +3,16 @@ import { Construct } from "constructs";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
 import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
 import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+
+
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
     
     
+    const unitTestOutput = new codepipeline.Artifact();
     const sourceOutput = new codepipeline.Artifact();
     
     const sourceAction = new codepipeline_actions.GitHubSourceAction({
@@ -19,6 +23,41 @@ export class PipelineCdkStack extends Stack {
       output: sourceOutput,
       branch: 'main', 
       });
+    
+    const pipeline = new codepipeline.Pipeline(this, 'CICD_Pipeline', {
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
+    });
+    
+    const codeQualityBuild = new codebuild.PipelineProject(this, 'Code Quality', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        privileged: true
+        },
+        buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml')
+    });
+    
+
+    
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [sourceAction],
+    });  
+    
+    pipeline.addStage({
+      stageName: "Code-Quality-Testing",
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: "Unit-Test",
+          project: codeQualityBuild,
+          input: sourceOutput,
+          outputs: [unitTestOutput],
+        }),
+      ],
+    });
+
+
+
       
   }
 }
