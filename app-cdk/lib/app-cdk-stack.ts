6b2a71d5f09d6b2d178291f90a76e614f9308cb7
lauradaybell <laura.l.daybell@gmail.com>
2022-10-28 18:41:59 +0000

    test

--- app-cdk/lib/app-cdk-stack.ts
+++ app-cdk/lib/app-cdk-stack.ts
@@ -6,11 +6,5 @@ export class AppCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
