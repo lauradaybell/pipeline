30f78115370d9967a0a79793bbcd9e63d98fcccd
lauradaybell <laura.l.daybell@gmail.com>
2022-10-28 20:14:03 +0000

    test

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,23 +1,16 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AppCdkStack } from '../lib/app-cdk-stack';
-import { PipelineCdkStack } from '../lib/pipeline-cdk-stack';
-import { EcrCdkStack } from '../lib/ecr-cdk-stack';
-
-
-
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { AppCdkStack } from "../lib/app-cdk-stack";
+import { PipelineCdkStack } from "../lib/pipeline-cdk-stack";
+import { EcrCdkStack } from "../lib/ecr-cdk-stack";
 
 const app = new cdk.App();
 
-const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+const ecrCdkStack = new EcrCdkStack(app, "ecr-stack", {});
 
+new AppCdkStack(app, "AppCdkStack", {});
 
-const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', { 
+const pipelineCdkStack = new PipelineCdkStack(app, "pipeline-stack", {
   ecrRepository: ecrCdkStack.repository,
 });
-
-
-new AppCdkStack(app, 'AppCdkStack', {});
-
-
