bddb413ca75e3bbd350fb6e8f930f85c99387b04
lauradaybell <laura.l.daybell@gmail.com>
2022-10-28 20:25:55 +0000

    docker

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -5,6 +5,7 @@ import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
 import * as codebuild from "aws-cdk-lib/aws-codebuild";
 import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
 import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 interface ConsumerProps extends StackProps {
   ecrRepository: ecr.Repository;
@@ -36,8 +37,49 @@ export class PipelineCdkStack extends Stack {
       }
     );
 
+    const dockerBuildProject = new codebuild.PipelineProject(
+      this,
+      "DockerBuildProject",
+      {
+        environmentVariables: {
+          IMAGE_TAG: { value: "latest" },
+          IMAGE_REPO_URI: { value: props.ecrRepository.repositoryUri },
+          AWS_DEFAULT_REGION: { value: process.env.CDK_DEFAULT_REGION },
+        },
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+          privileged: true,
+        },
+        buildSpec: codebuild.BuildSpec.fromSourceFilename(
+          "buildspec_docker.yml"
+        ),
+      }
+    );
+
+    const dockerBuildRolePolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ["*"],
+      actions: [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:BatchGetImage",
+        "ecr:InitiateLayerUpload",
+        "ecr:UploadLayerPart",
+        "ecr:CompleteLayerUpload",
+        "ecr:PutImage",
+      ],
+    });
+
+    dockerBuildProject.addToRolePolicy(dockerBuildRolePolicy);
+
     const sourceOutput = new codepipeline.Artifact();
     const unitTestOutput = new codepipeline.Artifact();
+    const dockerBuildOutput = new codepipeline.Artifact();
 
     pipeline.addStage({
       stageName: "Source",
@@ -63,8 +105,21 @@ export class PipelineCdkStack extends Stack {
       ],
     });
 
+    pipeline.addStage({
+      stageName: "Docker-Push-ECR",
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: "docker-build",
+          project: dockerBuildProject,
+          input: sourceOutput,
+          outputs: [dockerBuildOutput],
+        }),
+      ],
+    });
+
     new CfnOutput(this, "CodeCommitRepositoryUrl", {
       value: sourceRepo.repositoryCloneUrlHttp,
     });
   }
 }
+
