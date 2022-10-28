a29b2c04649fbbba4a0cc47c5e35006bf380a8f0
lauradaybell <laura.l.daybell@gmail.com>
2022-10-28 21:28:43 +0000

    updates

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -9,7 +9,8 @@ const app = new cdk.App();
 
 const ecrCdkStack = new EcrCdkStack(app, "ecr-stack", {});
 
-new AppCdkStack(app, "AppCdkStack", {});
+
+const testAppCdkStack = new AppCdkStack(app, 'test', { ecrRepository: ecrCdkStack.repository});
 
 const pipelineCdkStack = new PipelineCdkStack(app, "pipeline-stack", {
   ecrRepository: ecrCdkStack.repository,
