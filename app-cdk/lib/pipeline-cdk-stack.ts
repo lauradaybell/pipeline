1fc54765594621f3bb6665959f5776ddd2106340
lauradaybell <laura.l.daybell@gmail.com>
2022-10-28 21:06:58 +0000

    test

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,60 +1,60 @@
-import { CfnOutput, Stack, StackProps, SecretValue } from "aws-cdk-lib";
+import { Stack, StackProps, SecretValue } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as codecommit from "aws-cdk-lib/aws-codecommit";
 import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
-import * as codebuild from "aws-cdk-lib/aws-codebuild";
-import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
-import * as ecr from "aws-cdk-lib/aws-ecr";
-import * as iam from "aws-cdk-lib/aws-iam";
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
-interface ConsumerProps extends StackProps {
-  ecrRepository: ecr.Repository;
-}
+
+
 
 export class PipelineCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props: ConsumerProps) {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-
-
-    const pipeline = new codepipeline.Pipeline(this, "CICD_Pipeline", {
-      pipelineName: "CICD_Pipeline",
+    
+    
+    const unitTestOutput = new codepipeline.Artifact();
+    const sourceOutput = new codepipeline.Artifact();
+    
+    const sourceAction = new codepipeline_actions.GitHubSourceAction({
+      actionName: 'GitHub_Source',
+      owner: 'lauradaybell',
+      repo: 'pipeline',
+      oauthToken: SecretValue.secretsManager('pat'),
+      output: sourceOutput,
+      branch: 'main', 
+      });
+    
+    const pipeline = new codepipeline.Pipeline(this, 'CICD_Pipeline', {
+      pipelineName: 'CICD_Pipeline',
       crossAccountKeys: false,
     });
-
-    const codeQualityBuild = new codebuild.PipelineProject(
-      this,
-      "Code Quality",
-      {
-        environment: {
-          buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
-          privileged: true,
+    
+    const codeQualityBuild = new codebuild.PipelineProject(this, 'Code Quality', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        privileged: true
         },
-        buildSpec: codebuild.BuildSpec.fromSourceFilename("buildspec_test.yml"),
-      }
-    );
-
-    const dockerBuildProject = new codebuild.PipelineProject(
-      this,
-      "DockerBuildProject",
-      {
-        environmentVariables: {
-          IMAGE_TAG: { value: "latest" },
-          IMAGE_REPO_URI: { value: props.ecrRepository.repositoryUri },
-          AWS_DEFAULT_REGION: { value: process.env.CDK_DEFAULT_REGION },
+        buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml')
+    });
+    
+    const dockerBuildProject = new codebuild.PipelineProject(this, 'DockerBuildProject', {
+      environmentVariables: {
+        'IMAGE_TAG': { value: 'latest' },
+        'IMAGE_REPO_URI': {value: props.ecrRepository.repositoryUri },
+        'AWS_DEFAULT_REGION': {value: process.env.CDK_DEFAULT_REGION },
+      },
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        privileged: true
         },
-        environment: {
-          buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
-          privileged: true,
-        },
-        buildSpec: codebuild.BuildSpec.fromSourceFilename(
-          "buildspec_docker.yml"
-        ),
-      }
-    );
-
-    const dockerBuildRolePolicy = new iam.PolicyStatement({
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_docker.yml'),
+    });
+    
+    const dockerBuildRolePolicy =  new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
-      resources: ["*"],
+      resources: ['*'],
       actions: [
         "ecr:GetAuthorizationToken",
         "ecr:BatchCheckLayerAvailability",
@@ -67,32 +67,20 @@ export class PipelineCdkStack extends Stack {
         "ecr:InitiateLayerUpload",
         "ecr:UploadLayerPart",
         "ecr:CompleteLayerUpload",
-        "ecr:PutImage",
-      ],
+        "ecr:PutImage"
+      ]
     });
 
     dockerBuildProject.addToRolePolicy(dockerBuildRolePolicy);
-
-    const sourceOutput = new codepipeline.Artifact();
-    const unitTestOutput = new codepipeline.Artifact();
+    
     const dockerBuildOutput = new codepipeline.Artifact();
-    
-    
-    const sourceAction = new codepipeline_actions.GitHubSourceAction({
-      actionName: 'GitHub_Source',
-      owner: 'lauradaybell',
-      repo: 'pipeline',
-      oauthToken: SecretValue.secretsManager('pat'),
-      output: sourceOutput,
-      branch: 'main', 
-      });
-      
 
+    
     pipeline.addStage({
       stageName: 'Source',
       actions: [sourceAction],
     });  
-
+    
     pipeline.addStage({
       stageName: "Code-Quality-Testing",
       actions: [
@@ -104,18 +92,18 @@ export class PipelineCdkStack extends Stack {
         }),
       ],
     });
-
+    
     pipeline.addStage({
-      stageName: "Docker-Push-ECR",
+      stageName: 'Docker-Push-ECR',
       actions: [
         new codepipeline_actions.CodeBuildAction({
-          actionName: "docker-build",
+          actionName: 'docker-build',
           project: dockerBuildProject,
           input: sourceOutput,
           outputs: [dockerBuildOutput],
         }),
       ],
     });
+
   }
 }
-
