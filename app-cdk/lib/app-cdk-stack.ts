d6fdae4f962b07d69fc2704612860c323c934dc7
lauradaybell <laura.l.daybell@gmail.com>
2022-10-28 21:29:00 +0000

    updates

--- app-cdk/lib/app-cdk-stack.ts
+++ app-cdk/lib/app-cdk-stack.ts
@@ -1,10 +1,41 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
 
-export class AppCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+interface ConsumerProps extends StackProps {
+  ecrRepository: ecr.Repository;
+}
 
+export class AppCdkStack extends Stack {
+  public readonly fargateService: ecsPatterns.ApplicationLoadBalancedFargateService;
+
+  constructor(scope: Construct, id: string, props: ConsumerProps) {
+    super(scope, `${id}-app-stack`, props);
+
+    const vpc = new ec2.Vpc(this, `${id}Vpc`);
+
+    const cluster = new ecs.Cluster(this, `${id}EcsCluster`, {
+      vpc: vpc,
+    });
+
+    this.fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(
+      this,
+      `${id}FargateService`,
+      {
+        cluster: cluster,
+        publicLoadBalancer: true,
+        memoryLimitMiB: 1024,
+        cpu: 512,
+        desiredCount: 1,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromEcrRepository(props.ecrRepository),
+          containerName: 'my-app',
+          containerPort: 3000,
+        },
+      }
+    );
   }
 }
